743ea49fb384c30c6f1663712facae37cf8f889c
Oludire Damilare <oludire@gmail.com>
2025-02-16 20:59:50 +0100

    ✅🏆 FEAT: react-hook-form: step 1_register input fields

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,7 @@ export async function getCabins() {
   const { data: cabins, error } = await supabase.from('cabins').select('*');
 
   if (error) {
-    console.error(error);
+    // console.error(error);
     throw new Error('Cabins could not be loaded');
   }
   return cabins;
@@ -14,7 +14,7 @@ export async function deleteCabin(id) {
   const { data: cabins, error } = await supabase.from('cabins').delete().eq('id', id);
 
   if (error) {
-    console.error(error);
+    // console.error(error);
     throw new Error('Cabin could not be deleted');
   }
   return cabins;
