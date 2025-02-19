9aa1b60afbb1cea1132dcc111d54904b59a4cca5
Oludire Damilare <oludire@gmail.com>
2025-02-19 17:20:10 +0100

    ✅uploading images to supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -12,42 +12,6 @@ import { createCabin } from '../../services/apiCabins';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 
-const FormRow2 = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
-
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
 
@@ -70,12 +34,12 @@ function CreateCabinForm() {
   });
 
   const onSubmit = data => {
-    // console.log(data);
-    mutate(data);
+    console.log({ ...data, image: data.image[0] });
+    mutate({ ...data, image: data.image[0] });
   };
 
   const onError = errors => {
-    console.log(errors);
+    // console.log(errors);
   };
 
   return (
@@ -150,18 +114,21 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow2>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow2>
+      <FormRow label={'Cabin photo'}>
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register('image', { required: 'This field is required' })}
+        />
+      </FormRow>
 
-      <FormRow2>
+      <FormRow>
         {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
         <Button disabled={isCreating}>Add cabin</Button>
-      </FormRow2>
+      </FormRow>
     </Form>
   );
 }
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data: cabins, error } = await supabase.from('cabins').select('*');
@@ -7,15 +7,38 @@ export async function getCabins() {
     // console.error(error);
     throw new Error('Cabins could not be loaded');
   }
+
   return cabins;
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from('cabins').insert([newCabin]).select();
+  const imageName = `${newCabin.image.name}`.replace('/', '');
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabins-images//${imageName}`;
+
+  // https://kslildlatomuctqsltnc.supabase.co/storage/v1/object/public/cabins-images/cabin-001.jpg
+
+  // 1. create cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
 
   if (error) {
     // console.error(error);
-    throw new Error('Cabin could not be deleted');
+    throw new Error('Cabin could not be created');
+  }
+
+  // 2. upload cabin image
+  const { error: storageError } = await supabase.storage
+    .from('cabins-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. delete cabin where error persists in uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+
+    console.error(storageError);
+    throw new Error('Cabin image could not be uploaded');
   }
   return data;
 }
@@ -24,8 +47,9 @@ export async function deleteCabin(id) {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
   if (error) {
-    // console.error(error);
+    console.error(error);
     throw new Error('Cabin could not be deleted');
   }
+
   return data;
 }
