2effbbdb38556e4171763e44980f268b9bc049fb
Oludire Damilare <oludire@gmail.com>
2025-03-01 22:42:06 +0100

    ✅👌 IMPROVE: resusable table ep.2 - render body data with render props pattern

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -22,9 +22,8 @@ function CabinTable() {
         <div>Discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map(cabin => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+
+      <Table.Body data={cabins} render={cabin => <CabinRow cabin={cabin} key={cabin.id} />} />
     </Table>
   );
 }
@@ -34,3 +33,6 @@ function CabinTable() {
 }; */
 
 export default CabinTable;
+
+// {cabins.map(
+// ))}
