d4591dbd822c882cfdab0659bee93e86579ce4cd
Oludire Damilare <oludire@gmail.com>
2025-03-29 16:39:40 +0100

    ✅build dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 
+import ProtectedRoute from './ui/ProtectedRoute';
 import AppLayout from './ui/AppLayout';
+
+import { ThemeProvider } from './context/ThemeContext';
 
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
@@ -25,8 +28,6 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Users from './pages/Users';
 import PageNotFound from './pages/PageNotFound';
-import ProtectedRoute from './ui/ProtectedRoute';
-import { ThemeProvider } from './context/ThemeContext';
 
 /* const router = createBrowserRouter([
   {
